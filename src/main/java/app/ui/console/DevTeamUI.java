12686663ed8199fbb272ba75fce7e3cbe8f9a070
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-04-20 10:32:02 +0100

    dev [US-10] [CODE]

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -14,7 +14,7 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Student Name 1 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t Diogo Napoles - 1211155@isep.ipp.pt \n");
         System.out.printf("\t Student Name 2 - 120XXXX@isep.ipp.pt \n");
         System.out.printf("\t Student Name 3 - 120XXXX@isep.ipp.pt \n");
         System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
