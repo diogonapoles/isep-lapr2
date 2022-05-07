fe50d74af76c63ec43e51ac57c6f03a21cfe19ae
carol <1211638@isep.ipp.pt>
2022-05-07 23:51:12 +0100

    Fixing mistakes [US13][Implementation][ISSUE #35]

--- src/main/java/app/ui/console/NewVaccineUI.java
+++ src/main/java/app/ui/console/NewVaccineUI.java
@@ -12,26 +12,38 @@ public class NewVaccineUI implements Runnable {
     }
 
     public void run() {
-        getData();
-        if (Utils.confirm("Confirms data?(y/n)")) {
-            controller.newVaccine();
-            System.out.println("Vaccine registered successfully.");
-        } else
-            System.out.println("not a valid vaccine / already exists");
-    }
+            if(inputData())
+            {
+                getData();
 
-    private boolean inputData() {
-        String name = Utils.readLineFromConsole("Name:");
-        String brand = Utils.readLineFromConsole("Brand:");
-        String ageGroup = Utils.readLineFromConsole("Age Group:");
-        String doseNumber = Utils.readLineFromConsole("Dose Number:");
-        double dosage = Double.parseDouble(Utils.readLineFromConsole("Dosage:"));
-        int timeSinceLastDose = Integer.parseInt(Utils.readLineFromConsole("Time Since Last Dose:"));
+                if(Utils.confirm("Confirm data?(s/n)"))
+                {
+                    controller.registerVaccine();
+                    System.out.println("Vaccine registered successfully.");
+                }
+                else
+                    run();
+            }
+            else
+            {
+                System.out.println("Not a valid Vaccine or already exists");
+            }
+        }
 
-        return controller.newVaccine(name, brand, ageGroup, doseNumber, dosage, timeSinceLastDose);
+        private boolean inputData () {
+            String name = Utils.readLineFromConsole("Name:");
+            String brand = Utils.readLineFromConsole("Brand:");
+            String ageGroup = Utils.readLineFromConsole("Age Group:");
+            String doseNumber = Utils.readLineFromConsole("Dose Number:");
+            double dosage = Double.parseDouble(Utils.readLineFromConsole("Dosage:"));
+            int timeSinceLastDose = Integer.parseInt(Utils.readLineFromConsole("Time Since Last Dose:"));
+
+            return controller.newVaccine(name, brand, ageGroup, doseNumber, dosage, timeSinceLastDose);
+        }
+
 
         private void getData() {
             System.out.println(controller.getNewVaccinetoString());
         }
     }
-}
+
