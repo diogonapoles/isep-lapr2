453de5df5119c14d516c874e452f64327b6c0cb8
Gustavo Caiano <1210983@isep.ipp.pt>
2022-05-08 21:44:52 +0100

    test[US11]

--- src/test/java/app/controller/ListEmployeesControllerTest.java
+++ src/test/java/app/controller/ListEmployeesControllerTest.java
@@ -1,6 +1,9 @@
 package app.controller;
 
+import app.domain.model.CenterCoordinator;
 import app.domain.model.Employee;
+import app.domain.model.Nurse;
+import app.domain.model.Receptionist;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -13,7 +16,7 @@ public class ListEmployeesControllerTest {
             "teste@isep.ipp.pt", "12341234"){};
 
     ListEmployeesController controller = new ListEmployeesController();
-
+    RegisterEmployeeController emp = new RegisterEmployeeController();
 
     @Test
     public void getEmployeeRoles() {
@@ -28,5 +31,30 @@ public class ListEmployeesControllerTest {
     }
     @Test
     public void getEmployeesByRole() {
+
+        Receptionist emp1 = new Receptionist("maria", "123456789", "tgr",
+                "1120947@isep.ipp.pt", "12341234");
+        Receptionist emp4 = new Receptionist("lucas", "123987669", "rua santos lessa",
+                "1120985@isep.ipp.pt", "13562763");
+        Nurse emp2 = new Nurse("joao", "912834739", "tigas streat",
+                "1243555@isep.ipp.pt", "12345678");
+        CenterCoordinator emp3 = new CenterCoordinator ("camila", "812834567", "avenida da liberdade",
+                "1238146@isep.ipp.pt", "12344678");
+
+
+        List<Employee> rolesReceptionist = new ArrayList<>();
+        rolesReceptionist.add(emp1);
+        rolesReceptionist.add(emp4);
+        List<Employee> rolesNurse = new ArrayList<>();
+        rolesNurse.add(emp2);
+        List<Employee> rolesCoordinator = new ArrayList<>();
+        rolesCoordinator.add(emp3);
+
+
+
+
+        assertEquals( rolesReceptionist, controller.getEmployeesByRole(0));
+
+
     }
 }
