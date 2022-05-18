6bc546a83d6a3d38d82df6fc3ce443d5a80a94f8
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-05-18 10:50:20 +0100

    JavaDOC#1[US14][Implementation]

--- src/main/java/app/controller/LoadSNSUsersController.java
+++ src/main/java/app/controller/LoadSNSUsersController.java
@@ -7,23 +7,46 @@ import app.domain.model.VaccineType;
 
 import java.util.List;
 
+/**
+ * The type Load sns users controller.
+ */
 public class LoadSNSUsersController {
     private App oApp;
     private Company oCompany;
     private SNSUser oSNSUser;
     public List <SNSUser> oSNSUserList;
 
+    /**
+     * Instantiates a new Load sns users controller.
+     */
     public LoadSNSUsersController(){
         this.oApp = App.getInstance();
         this.oCompany = oApp.getCompany();
     }
 
+    /**
+     * Get CSV file types list.
+     *
+     * @return the list
+     */
     public List<String> getCSVFileTypes(){return this.oCompany.getCSVReader().getCSVFileTypes();}
 
+    /**
+     * Import SNS User CSV boolean.
+     *
+     * @return the boolean
+     */
     public boolean importSNSUserCSV() {
         return this.oCompany.getSNSUserStore().importSNSUserCSV(oSNSUserList);
     }
 
+    /**
+     * New CSV reader boolean.
+     *
+     * @param typeSelection the type selection
+     * @param fileLocation  the file location
+     * @return the boolean
+     */
     public boolean newCSVReader(int typeSelection, String fileLocation) {
         this.oSNSUserList = oCompany.getCSVReader().readCSVFile(typeSelection, fileLocation);
         if (this.oSNSUserList != null)
@@ -32,6 +55,9 @@ public class LoadSNSUsersController {
             return false;
     }
 
+    /**
+     * Clear temp array.
+     */
     public void clearTempArray(){
         oSNSUserList.clear();
     }
