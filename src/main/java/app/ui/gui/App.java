e9052df552ea456ce1527f69daaba2bebea51e97
Gustavo Caiano <1210983@isep.ipp.pt>
2022-06-07 11:52:59 +0100

    JavaFx Main menu[SprintD][Implementation]

---
+++ src/main/java/app/ui/gui/App.java
@@ -0,0 +1,81 @@
+package app.ui.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.awt.*;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App extends Application {
+    private Stage stage;
+    private final double MINIMUM_WINDOW_WIDTH = 400.0;
+    private final double MINIMUM_WINDOW_HEIGHT = 300.0;
+    private final double SCENE_WIDTH = 450.0;
+    private final double SCENE_HEIGHT = 350.0;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+        public void start(Stage stage) throws Exception {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("/fxml/Main.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add("/styles/Styles.css");
+
+
+
+            /*Image img = new Im;
+            stage.getIcons().add(img);*/
+            stage.setTitle("LAPR2APP");
+            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+
+        }
+
+
+/*
+    public void toMainScene() {
+        try {
+            MainUI mainUI = (MainUI) replaceSceneContent("fxml/Main.fxml");
+            mainUI.setMainApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = App.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(App.class.getResource(fxml));
+        Pane page;
+        try {
+            page = (Pane) loader.load(in);
+        } finally {
+            in.close();
+        }
+        Scene scene = new Scene(page, SCENE_WIDTH, SCENE_HEIGHT);
+        scene.getStylesheets().add("/styles/Styles.css");
+        this.stage.setScene(scene);
+        this.stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
+*/
+
+}
---
+++ src/main/java/app/ui/gui/MainUI.java
@@ -0,0 +1,14 @@
+package app.ui.gui;
+
+
+import javafx.application.Application;
+
+public class MainUI extends App {
+    private App mainApp;
+
+
+    public void setMainApp(App mainApp) {
+        this.mainApp = mainApp;
+
+    }
+}
