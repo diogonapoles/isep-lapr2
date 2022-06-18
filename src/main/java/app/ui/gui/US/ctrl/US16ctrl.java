5f96b93010897cc4165cece6f9e598f289a42361
Gustavo Caiano <1210983@isep.ipp.pt>
2022-06-18 01:52:17 +0100

    [JavaFx][US16][Implementation]

--- src/main/java/app/ui/gui/US/ctrl/US16ctrl.java
+++ src/main/java/app/ui/gui/US/ctrl/US16ctrl.java
@@ -2,12 +2,15 @@ package app.ui.gui.US.ctrl;
 
 
 import app.controller.App;
+import app.domain.model.Benchmark;
 import app.domain.model.Company;
 import app.domain.model.vaccinationCenter.VaccinationCenter;
+import app.domain.shared.BruteForce;
 import app.ui.gui.roles.CenterCoordinatorUI;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -15,42 +18,49 @@ public class US16ctrl {
 
     private App oApp;
     private Company oCompany;
-    private VaccinationCenter vaccinationCenter;
+    private VaccinationCenter vaccinationCenter = CenterCoordinatorUI.vaccinationCenter;
+    private BruteForce bruteForceAlgorithm;
+    private Benchmark benchmarkAlgorithm;
 
     public US16ctrl() {
         this.oApp = App.getInstance();
         this.oCompany = oApp.getCompany();
+        this.bruteForceAlgorithm = new BruteForce(oCompany);
+        this.benchmarkAlgorithm = new Benchmark();
     }
 
+
     public VaccinationCenter getWorking() {
-        vaccinationCenter= CenterCoordinatorUI.vaccinationCenter;
-        //vaccinationCenter = oCompany.getEmployeeStore().getWorking(oApp.getCurrentUserSession().getUserId().getEmail());
         return vaccinationCenter;
     }
 
-    public boolean validateTimeIntervalForVaccinationCenter(int timeInterval, String start, String end){
+    public void setWorking(VaccinationCenter vc) {
+        vaccinationCenter = vc;
+    }
+
+    public boolean validateTimeIntervalForVaccinationCenter(int timeInterval){
         return vaccinationCenter.validateTimeIntervalForVaccinationCenter(timeInterval);
     }
 
-    public Date setTime(String time, boolean flag){
-        return vaccinationCenter.analyzePerformanceTime(time, flag);
+    public int[] createInputList(int timeInterval, String day){
+        String startStr = "08:00";
+        Date start = stringToFullDate(day.concat(" ").concat(startStr));
+
+        return bruteForceAlgorithm.createInputList(timeInterval, start);
     }
 
-    public int[] createInputList(int timeInterval, String startString, String endString){
-        Date start = stringToDate(startString);
-        Date end = stringToDate(endString);
-
-        if (start == null || end == null){
-            throw new IllegalArgumentException("Invalid Dates");
-        }
-
-        return oCompany.getBruteForceAlgorithm().createInputList(timeInterval, start);
+    public int[] getMaxSubArrayBruteForce(int[] input){
+        return bruteForceAlgorithm.maxSubArray(input);
     }
 
-    public Date stringToDate(String strDate) {
+    public String findDay(){
+        return bruteForceAlgorithm.findDay(oCompany.getLegacySystemData().getArrivalList());
+    }
+
+    public Date stringToFullDate(String strDate) {
         Date date;
         try {
-            SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
             df.setLenient(false);
             date = df.parse(strDate);
         } catch (ParseException e) {
@@ -60,19 +70,55 @@ public class US16ctrl {
     }
 
     public void printArray(int[] array){
-        System.out.println("[");
+        System.out.print("[");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
+            if (i != array.length-1){
+                System.out.print(", ");
+            }
         }
         System.out.print("]");
         System.out.println();
     }
 
     public int[] getMaxSubArrayBenchmark(int[] inputList) {
-        return oCompany.getBenchmarkAlgorithm().max(inputList);
+        return benchmarkAlgorithm.max(inputList);
     }
 
-    public int getMaxSumBruteBenchmark(int[] maxSubArray) {
-        return oCompany.getBenchmarkAlgorithm().sum(maxSubArray);
+    public int getMaxSumBenchmark(int[] maxSubArray) {
+        return benchmarkAlgorithm.sum(maxSubArray);
+    }
+
+    public int[] findMaxSubarray(int[] inputList, int i, int j) {
+        int[] subArray = new int[j-i];
+        int pos = i;
+        for (int k = 0; k < j; k++) {
+            subArray[k] = inputList[pos];
+            pos++;
+        }
+        return subArray;
+    }
+
+    public String findTimeInterval(String day, int timeInterval, int i, int length) {
+        try {
+            String start = "08:00";
+            SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+            Date d1 = df.parse(start);
+            Calendar s = Calendar.getInstance();
+            s.setTime(d1);
+            s.add(Calendar.MINUTE, timeInterval*i);
+            String startTime = df.format(s.getTime());
+
+            Calendar e = Calendar.getInstance();
+            e.setTime(d1);
+            e.add(Calendar.MINUTE, timeInterval*length);
+            String endTime = df.format(e.getTime());
+
+            return "[" + day + " " + startTime + ", " + day + " " + endTime + "]";
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/main/java/app/ui/gui/US/ui/US16ui.java
+++ src/main/java/app/ui/gui/US/ui/US16ui.java
@@ -20,174 +20,33 @@ import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Properties;
 
 public class US16ui {
     private final US16ctrl controller;
     private Stage stage;
-    private Appfx mainAppfx;
     @FXML
-    private TextField dateTF;
+    private Button btnBack;
 
     @FXML
-    private TextField endTF;
+    private Button btnConfirm;
 
     @FXML
     private TextField intervalTF;
 
     @FXML
-    private TextField startTF;
-
-    @FXML
     private Button btnOK1;
+
     @FXML
     private Button btnOK2;
 
-    public void setMainApp(Appfx mainAppfx) {
-        this.mainAppfx = mainAppfx;
 
+
+    public US16ui() {
+        controller = new US16ctrl();
     }
-
-
-
-
-        @FXML
-        private Button btnBack;
-
-        @FXML
-        private Button btnConfirm;
-
-
-        @FXML
-         void btnConfirm(ActionEvent event) {
-            boolean success =true;
-            if (controller.getWorking() == null) {
-                System.out.println("Doesn't exist");
-                return;
-            }
-            Properties props = new Properties();
-            try {
-                props.load(new FileInputStream("./config.properties"));
-            } catch (IOException e) {
-                System.out.println("Could not read config file");
-                return;
-            }
-            String dateString = dateTF.getText();
-             Date date = controller.stringToDate(dateString);
-            if (date == null){
-                try {
-                    var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s3.fxml"));
-                    Parent root = loader.load();
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
-                    String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
-                    scene.getStylesheets().add(css);
-                    stage.setScene(scene);
-
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }
-                success = false;
-            }
-
-            String start = startTF.getText();
-            if (!start.matches("\\d{2}:\\d{2}")){
-                try {
-                    var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s3.fxml"));
-                    Parent root = loader.load();
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
-                    String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
-                    scene.getStylesheets().add(css);
-                    stage.setScene(scene);
-
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }
-                success = false;
-            }
-            String end = endTF.getText();
-            if (!end.matches("\\d{2}:\\d{2}")){
-                try {
-                    var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s3.fxml"));
-                    Parent root = loader.load();
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
-                    String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
-                    scene.getStylesheets().add(css);
-                    stage.setScene(scene);
-
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }
-                success = false;
-            }
-
-                int timeInterval = Integer.parseInt(intervalTF.getText());
-                if (!controller.validateTimeIntervalForVaccinationCenter(timeInterval, start, end)) {
-                    try {
-                        var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s3.fxml"));
-                        Parent root = loader.load();
-                        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                        Scene scene = new Scene(root);
-                        // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
-                        String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
-                        scene.getStylesheets().add(css);
-                        stage.setScene(scene);
-
-                    }catch (Exception ex){
-                        ex.printStackTrace();
-                    }
-                    success = false;
-                }
-
-
-            String algorithm = props.getProperty(Constants.PARAMS_ALGORITHM);
-            int[] inputList = controller.createInputList(timeInterval, start, end);
-            if (algorithm.equals(BRUTEFORCE_ALGORITHM)) {
-                /*//int[] maxSubArray = controller.getMaxSubArrayBruteForce(inputList);
-                int maxSum = controller.getMaxSumBruteForce(maxSubArray);
-                System.out.println("Input List:");
-                controller.printArray(inputList);
-
-                System.out.println("Maximum Subarray:");
-                controller.printArray(maxSubArray);
-
-                System.out.println("Max Sum:" + maxSum);*/
-            }
-            if (algorithm.equals(BENCHMARK_ALGORITHM)) {
-                int[] maxSubArray = controller.getMaxSubArrayBenchmark(inputList);
-                int maxSum = controller.getMaxSumBruteBenchmark(maxSubArray);
-                System.out.println("Input List:");
-                controller.printArray(inputList);
-
-                System.out.println("Maximum Subarray:");
-                controller.printArray(maxSubArray);
-
-                System.out.println("Max Sum:" + maxSum);
-            }
-            if(success){
-                try {
-                    var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s2.fxml"));
-                    Parent root = loader.load();
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
-                    String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
-                    scene.getStylesheets().add(css);
-                    stage.setScene(scene);
-
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }
-            }
-
-
-        }
     @FXML
     public void btnOK2(ActionEvent event) throws Exception {
 
@@ -209,7 +68,7 @@ public class US16ui {
 
 
     }
-@FXML
+    @FXML
     public void btnOK1(ActionEvent event) {
         try {
             var loader = new FXMLLoader(getClass().getResource("/fxml/CenterCoordinator.fxml"));
@@ -226,10 +85,52 @@ public class US16ui {
         }
     }
 
-        @FXML
-        void btnBack(ActionEvent event) {
+    @FXML
+    void btnBack(ActionEvent event) {
+        try {
+            var loader = new FXMLLoader(getClass().getResource("/fxml/CenterCoordinator.fxml"));
+            Parent root = loader.load();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
+            String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    void btnConfirm(ActionEvent event) {
+        boolean success=true;
+        if (controller.getWorking() == null) {
+            System.out.println("Doesn't exist");
+            return;
+        }
+
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            System.out.println("Could not read config file");
+            return;
+        }
+
+        String day;
+        try{
+            day = controller.findDay();
+        }catch (Exception e){
+            System.out.println("You have to load a legacy file first");
+            return;
+        }
+
+
+        int timeInterval = Utils.readIntegerFromConsole("Type the desired time interval (in minutes):");
+        if (!controller.validateTimeIntervalForVaccinationCenter(timeInterval)) {
             try {
-                var loader = new FXMLLoader(getClass().getResource("/fxml/CenterCoordinator.fxml"));
+                var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s3.fxml"));
                 Parent root = loader.load();
                 stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 Scene scene = new Scene(root);
@@ -241,34 +142,61 @@ public class US16ui {
             }catch (Exception ex){
                 ex.printStackTrace();
             }
+            return;
         }
 
+        String algorithm = props.getProperty(Constants.PARAMS_ALGORITHM);
 
 
+        int[] inputList = controller.createInputList(timeInterval, day);
+        if (algorithm.equals(BRUTEFORCE_ALGORITHM)) {
+            int[] maxInfo = controller.getMaxSubArrayBruteForce(inputList);
+            int[] maxSubArray = controller.findMaxSubarray(inputList, maxInfo[0], maxInfo[1]);
+            System.out.println("Input List:");
+            controller.printArray(inputList);
 
-    public US16ui() {
-        controller = new US16ctrl();
+            System.out.println("Maximum Subarray:");
+            controller.printArray(maxSubArray);
+
+            System.out.println("Max Sum:" + maxInfo[2]);
+
+            System.out.println("Time Interval:");
+            System.out.println(controller.findTimeInterval(day, timeInterval, maxInfo[0], maxSubArray.length));
+        }
+        if (algorithm.equals(BENCHMARK_ALGORITHM)) {
+            System.out.println("Input List:");
+            controller.printArray(inputList);
+
+            System.out.println("Maximum Subarray:");
+            int[] result = controller.getMaxSubArrayBenchmark(inputList);
+            System.out.println(Arrays.toString(result));
+
+            int maxSum = controller.getMaxSumBenchmark(result);
+
+            System.out.println("Max Sum:" + maxSum);
+        }
+        if(success){
+            try {
+                var loader = new FXMLLoader(getClass().getResource("/fxml/US/16s2.fxml"));
+                Parent root = loader.load();
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                // scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
+                String css = this.getClass().getResource("/styles/Styles.css").toExternalForm();
+                scene.getStylesheets().add(css);
+                stage.setScene(scene);
+
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+        }
+
     }
+
 
     private static final String BRUTEFORCE_ALGORITHM = "BruteForce";
     private static final String BENCHMARK_ALGORITHM = "Benchmark";
 
-    public void run() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
 }
